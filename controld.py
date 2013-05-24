ce9705c80ff3a1c8de6bf30f01498fe3b06ec50b
root <home>
2013-05-24 13:18:56 +0400

    controld

--- controld.py
+++ controld.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python
  
-import sys, time, socket, httplib2, datetime
+import os, sys, time, socket, httplib2, datetime
 from daemon import Daemon
 
 class MyDaemon(Daemon):
 	lircd_address = '/var/run/lirc/lircd'
 	noolite_command = 'noolite -api -'
-	mac_host='192.168.0.101'
+	mac_host = '192.168.0.101'
 	logfile = '/var/log/controld'
 
 	keys_light = {
-		"KEY_1" : "sw_ch 5",
-		"KEY_2" : "on_ch 5",
-		"KEY_3" : "set_ch 5 50"
+		"KEY_1" : "sw_ch 6",
+		"KEY_2" : "on_ch 6",
+		"KEY_3" : "set_ch 6 50"
 	}
 
 	def run(self):
 		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-		s.connect(lircd_address)
+		s.connect(self.lircd_address)
 		while True:
 			data = s.recv(1024)
-			log('Received', repr(data))
-			process(data)
+			if not data: 
+				self.log("Socket closed, exiting")
+				break
+			self.log("Received: " + repr(data))
+			self.process(data.strip())
 		s.close()
 
 	def log(self, message):
-		open(logfile, "a").write("[%s] %s\n" % (datetime.datetime.now(), message))
+		open(self.logfile, "a").write("[%s] %s\n" % (datetime.datetime.now(), message))
 
 	def process(self, key):
-		if key in keys_light:
-			os.system(noolite_command + keys_light[key])
-		else
-			r, content = httplib2.Http().request("http://" + mac_host + "/remote?key=" + key)
-			if content != ok:
-				log("Error on key %s: %s" % (key, content))
-
-
+		if key in self.keys_light:
+			command = self.noolite_command + self.keys_light[key]
+			self.log("Executing " + repr(command))
+			code = os.system(command)
+			self.log("Return code: " + repr(code))
+		else:
+			url = "http://%s/remote?key=%s" % (self.mac_host, key)
+			self.log("Requesting " + repr(url))
+			r, content = httplib2.Http().request(url)
+			self.log("Response code: %s %s" % (r.status, r.reason))
+			#if content != "ok":
+			#	self.log("Error on key %s: %s" % (repr(key), repr(content)))
 
 
 
@@ -52,6 +59,8 @@ if __name__ == "__main__":
 			daemon.stop()
 		elif 'restart' == sys.argv[1]:
 			daemon.restart()
+                elif 'run' == sys.argv[1]:
+                        daemon.run()
 		else:
 			print "Unknown command"
 			sys.exit(2)
