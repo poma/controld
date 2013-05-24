604ddd51137b00b0f82d5fa88920edd9eb5a5147
root <home>
2013-05-24 14:23:09 +0400

    settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+lircd_socket = '/var/run/lirc/lircd'
+noolite_command = 'noolite -api -'
+http_host = '192.168.0.101'
+logfile = '/var/log/controld'
+pidfile = '/var/run/controld.pid'
+
+keys_light = {
+	"KEY_1" : "sw_ch 6",
+	"KEY_2" : "on_ch 6",
+	"KEY_3" : "set_ch 6 50"
+	}
