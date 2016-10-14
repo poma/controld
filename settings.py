c9446a6fd3a30899649a4e6329514b76da6e48cc
poma <Semenov.Roman@mail.ru>
2016-10-14 20:17:56 +0300

    update settings

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 lircd_socket = '/var/run/lirc/lircd'
 noolite_command = '/usr/local/bin/noolite -api -'
-http_host = '172.16.128.2:8080'
+http_host = '172.16.128.4:5112'
 logfile = '/var/log/controld'
 pidfile = '/var/run/controld.pid'
 
 keys_light = {
-	"KEY_RED" : ["sw_ch 5", "sw_ch 6", "sw_ch 7"],
-	"KEY_GREEN" : ["sw_ch 6"],
-	"KEY_YELLOW" : ["sw_ch 5"],
-	"KEY_BLUE" : ["sw_ch 7"],
-	"KEY_TITLE" : ["set_ch 5 50", "set_ch 6 50"],
-	"KEY_TEXT" : ["set_ch 5 100", "set_ch 6 100"],
-	"KEY_CLEAR" : ["off_ch 5", "off_ch 6", "off_ch 7"]
-	}
+        "KEY_RED" : ["set_ch 5 100", "set_ch 6 100"],
+        "KEY_GREEN" : ["set_ch 5 50", "set_ch 6 50"],
+        "KEY_YELLOW" : ["set_ch 5 30", "set_ch 6 30"],
+        "KEY_BLUE" : ["set_ch 5 20", "set_ch 6 20"],
+        "KEY_TITLE" : ["sw_ch 5"],
+        "KEY_TEXT" : ["sw_ch 6"],
+        "KEY_CLEAR" : ["off_ch 5", "off_ch 6"]
+        }
